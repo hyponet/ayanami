abf7b2f3b51d4b9fa68a899f24e6ff97143a01bc
Coderhypo <i@ihypo.net>
2016-01-28 12:29:20 +0800

    add src

---
+++ src/main/java/xyz/acmer/controller/HomeController.java
@@ -0,0 +1,16 @@
+package xyz.acmer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+}
