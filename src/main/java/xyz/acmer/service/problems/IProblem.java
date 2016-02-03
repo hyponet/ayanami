39168142a6a4b44418ac55850aaac252c90a94c9
Coderhypo <i@ihypo.net>
2016-02-03 17:32:18 +0800

    update

---
+++ src/main/java/xyz/acmer/service/problems/IProblem.java
@@ -0,0 +1,7 @@
+package xyz.acmer.service.problems;
+
+/**
+ * Created by hypo on 16-2-2.
+ */
+public interface IProblem {
+}
---
+++ src/main/java/xyz/acmer/service/users/IUser.java
@@ -0,0 +1,7 @@
+package xyz.acmer.service.users;
+
+/**
+ * Created by hypo on 16-2-2.
+ */
+public interface IUser {
+}
