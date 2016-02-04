5c5c38f10358016196981a38e46a8d5196c560e3
Coderhypo <i@ihypo.net>
2016-02-04 18:12:17 +0800

    add problem

--- src/main/java/xyz/acmer/service/problems/IProblem.java
+++ src/main/java/xyz/acmer/service/problems/IProblem.java
@@ -1,7 +1,21 @@
 package xyz.acmer.service.problems;
 
+import java.util.Date;
+
 /**
  * Created by hypo on 16-2-2.
  */
 public interface IProblem {
+    Integer getProblemId();
+    String getOjCode();
+    void setOjCode(String ojCode);
+    String getPid();
+    void setPid(String pid);
+    String getTitle();
+    void setTitle(String title);
+    Integer getAcceptedNumber();
+    void setAcceptedNumber(Integer acceptedNumber);
+    Integer getSubmitNumber();
+    void setSubmitNumber(Integer submitNumber);
+    Date getUpdateDate();
 }
--- src/main/java/xyz/acmer/service/problems/Problem.java
+++ src/main/java/xyz/acmer/service/problems/Problem.java
@@ -1,7 +1,76 @@
 package xyz.acmer.service.problems;
 
+import java.util.Date;
+
 /**
  * Created by hypo on 16-2-2.
  */
 public class Problem {
+    private Integer problemId;
+    private String ojCode;
+    private String pid;
+    private String title;
+    private Integer acceptedNumber;
+    private Integer submitNumber;
+    private Date updateDate;
+
+    public Problem(String ojCode, String pid, String title) {
+        this.ojCode = ojCode;
+        this.pid = pid;
+        this.title = title;
+        this.acceptedNumber = 0;
+        this.submitNumber = 0;
+    }
+
+    public Integer getProblemId() {
+        return problemId;
+    }
+
+    public String getOjCode() {
+        return ojCode;
+    }
+
+    public void setOjCode(String ojCode) {
+        this.ojCode = ojCode;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getAcceptedNumber() {
+        return acceptedNumber;
+    }
+
+    public void setAcceptedNumber(Integer acceptedNumber) {
+        this.acceptedNumber = acceptedNumber;
+    }
+
+    public Integer getSubmitNumber() {
+        return submitNumber;
+    }
+
+    public void setSubmitNumber(Integer submitNumber) {
+        this.submitNumber = submitNumber;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate() {
+        this.updateDate = new Date();
+    }
 }
