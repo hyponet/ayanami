883faf1a3a801e3fcf1e17edca80da76201a3d75
Coderhypo <i@ihypo.net>
2016-02-11 10:49:07 +0800

    add userinfo

--- src/main/java/xyz/acmer/service/users/UserInfo.java
+++ src/main/java/xyz/acmer/service/users/UserInfo.java
@@ -4,4 +4,122 @@ package xyz.acmer.service.users;
  * Created by hypo on 16-2-11.
  */
 public class UserInfo {
+    private Integer infoId;
+    private Integer userId;
+    private String webSite;
+    private String company;
+    private String school;
+    private String tagLine;
+    private String github;
+    private String psnId;
+    private String steamId;
+    private String telegram;
+    private String twitter;
+    private String codingNet;
+    private String bio;
+    private String feedUrl;
+
+    public Integer getInfoId() {
+        return infoId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public void setWebSite(String webSite) {
+        this.webSite = webSite;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getTagLine() {
+        return tagLine;
+    }
+
+    public void setTagLine(String tagLine) {
+        this.tagLine = tagLine;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getPsnId() {
+        return psnId;
+    }
+
+    public void setPsnId(String psnId) {
+        this.psnId = psnId;
+    }
+
+    public String getSteamId() {
+        return steamId;
+    }
+
+    public void setSteamId(String steamId) {
+        this.steamId = steamId;
+    }
+
+    public String getTelegram() {
+        return telegram;
+    }
+
+    public void setTelegram(String telegram) {
+        this.telegram = telegram;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getCodingNet() {
+        return codingNet;
+    }
+
+    public void setCodingNet(String codingNet) {
+        this.codingNet = codingNet;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getFeedUrl() {
+        return feedUrl;
+    }
+
+    public void setFeedUrl(String feedUrl) {
+        this.feedUrl = feedUrl;
+    }
 }
