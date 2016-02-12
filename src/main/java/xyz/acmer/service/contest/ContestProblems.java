9a6ada41e50ebc2d27c9fccf2036e5138bad4a23
Coderhypo <i@ihypo.net>
2016-02-12 10:10:06 +0800

    add ContestProblems

--- src/main/java/xyz/acmer/service/contest/ContestProblems.java
+++ src/main/java/xyz/acmer/service/contest/ContestProblems.java
@@ -4,4 +4,62 @@ package xyz.acmer.service.contest;
  * Created by hypo on 16-2-11.
  */
 public class ContestProblems {
+    private Long cpid;
+    private Integer problemId;
+    private Integer contestId;
+    private String title;
+    private Integer accepted;
+    private Integer submit;
+
+    public ContestProblems(Integer problemId, Integer contestId, String title) {
+        this.problemId = problemId;
+        this.contestId = contestId;
+        this.title = title;
+        this.accepted = 0;
+        this.submit = 0;
+    }
+
+    public Long getCpid() {
+        return cpid;
+    }
+
+    public Integer getProblemId() {
+        return problemId;
+    }
+
+    public void setProblemId(Integer problemId) {
+        this.problemId = problemId;
+    }
+
+    public Integer getContestId() {
+        return contestId;
+    }
+
+    public void setContestId(Integer contestId) {
+        this.contestId = contestId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(Integer accepted) {
+        this.accepted = accepted;
+    }
+
+    public Integer getSubmit() {
+        return submit;
+    }
+
+    public void setSubmit(Integer submit) {
+        this.submit = submit;
+    }
 }
