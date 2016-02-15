8a28d8edeb1d4a675a67424489a676ff292274bc
Coderhypo <i@ihypo.net>
2016-02-16 00:51:06 +0800

    finish problem entity

--- src/main/java/xyz/acmer/entity/problem/Problem.java
+++ src/main/java/xyz/acmer/entity/problem/Problem.java
@@ -1,31 +1,71 @@
 package xyz.acmer.entity.problem;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
 /**
+ * 题目信息表
  * Created by hypo on 16-2-2.
  */
+@Entity
 public class Problem {
+
+    /**
+     * 站内统一 pid
+     */
     private Integer problemId;
+
+    /**
+     * OJ代号
+     */
     private String ojCode;
+
+    /**
+     * 该OJ下的pid
+     * 之所以使用String是因为部分OJ的pid包含字母
+     */
     private String pid;
+
+    /**
+     * 题目标题
+     * 在ayanami下，只存储标题，而内容等由makinami负责存储以及维护
+     */
     private String title;
+
+    /**
+     * 在该OJ下AC数
+     */
     private Integer acceptedNumber;
+
+    /**
+     * 在该OJ下提交数
+     */
     private Integer submitNumber;
+
+    /**
+     * 抓取该题目的更新时间
+     */
     private Date updateDate;
 
-    public Problem(String ojCode, String pid, String title) {
+    public Problem(String ojCode, String pid, String title, Integer acceptedNumber, Integer submitNumber) {
         this.ojCode = ojCode;
         this.pid = pid;
         this.title = title;
-        this.acceptedNumber = 0;
-        this.submitNumber = 0;
+        this.acceptedNumber = acceptedNumber;
+        this.submitNumber = submitNumber;
+        this.updateDate = new Date();
     }
 
+    @Id
+    @GeneratedValue
     public Integer getProblemId() {
         return problemId;
     }
 
+    @Column(name = "oj_code", nullable = false, length = 10)
     public String getOjCode() {
         return ojCode;
     }
@@ -34,6 +74,7 @@ public class Problem {
         this.ojCode = ojCode;
     }
 
+    @Column(name = "pid", nullable = false, length = 20)
     public String getPid() {
         return pid;
     }
@@ -42,6 +83,7 @@ public class Problem {
         this.pid = pid;
     }
 
+    @Column(name = "title", nullable = false)
     public String getTitle() {
         return title;
     }
@@ -50,6 +92,7 @@ public class Problem {
         this.title = title;
     }
 
+    @Column(name = "accepted", nullable = false)
     public Integer getAcceptedNumber() {
         return acceptedNumber;
     }
@@ -58,6 +101,7 @@ public class Problem {
         this.acceptedNumber = acceptedNumber;
     }
 
+    @Column(name = "submit", nullable = false)
     public Integer getSubmitNumber() {
         return submitNumber;
     }
@@ -66,6 +110,7 @@ public class Problem {
         this.submitNumber = submitNumber;
     }
 
+    @Column(name = "update_date", nullable = false, columnDefinition = "TIMESTAMP")
     public Date getUpdateDate() {
         return updateDate;
     }
