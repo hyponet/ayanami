db3da150eaa13ff46956de30d6004a5a0a8e7152
Coderhypo <i@ihypo.net>
2016-02-16 00:09:19 +0800

    update StringHelper

--- src/main/java/xyz/acmer/tool/StringHelper.java
+++ src/main/java/xyz/acmer/tool/StringHelper.java
@@ -1,7 +1,15 @@
 package xyz.acmer.tool;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by hypo on 16-2-15.
  */
 public class StringHelper {
+
+    public String getCurrentDateTime(Date date){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return sdf.format(date);
+    }
 }
