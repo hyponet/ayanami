72f04f9a28d1cf57be49671975d739de63ef89cf
Coderhypo <i@ihypo.net>
2016-02-15 23:03:41 +0800

    update user dao

--- src/main/java/xyz/acmer/dao/impl/UserDao.java
+++ src/main/java/xyz/acmer/dao/impl/UserDao.java
@@ -1,9 +1,9 @@
 package xyz.acmer.dao.impl;
 
-import xyz.acmer.dao.IDao;
+import xyz.acmer.dao.IUserDao;
 
 /**
  * Created by hypo on 16-2-8.
  */
-public class UserDao implements IDao {
+public class UserDao implements IUserDao {
 }
