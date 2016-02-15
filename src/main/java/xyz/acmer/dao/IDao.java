f945ca993c28241bbb48826e1d40bd9963ac935c
Coderhypo <i@ihypo.net>
2016-02-15 23:02:58 +0800

    update

--- src/main/java/xyz/acmer/dao/IDao.java
+++
@@ -1,7 +0,0 @@
-package xyz.acmer.dao;
-
-/**
- * Created by hypo on 16-2-6.
- */
-public interface IDao {
-}
---
+++ src/main/java/xyz/acmer/dao/IUserDao.java
@@ -0,0 +1,7 @@
+package xyz.acmer.dao;
+
+/**
+ * Created by hypo on 16-2-15.
+ */
+public class IUserDao {
+}
