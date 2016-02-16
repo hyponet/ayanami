df2b3243e5ca286ca5230c0590521d9185b88b7e
Coderhypo <i@ihypo.net>
2016-02-17 00:11:44 +0800

    finish ContestInfo entity

--- src/main/java/xyz/acmer/entity/contest/ContestInfo.java
+++ src/main/java/xyz/acmer/entity/contest/ContestInfo.java
@@ -1,45 +1,69 @@
 package xyz.acmer.entity.contest;
 
+import xyz.acmer.entity.user.User;
+
+import javax.persistence.*;
 import java.util.Date;
 
 /**
+ * 比赛信息表
  * Created by hypo on 16-2-11.
  */
+@Entity
 public class ContestInfo {
+
     private Integer contestId;
-    private Integer createrId;
+    private User creater;
     private String title;
+
+    /**
+     * 比赛开始时间
+     * 比赛长度
+     * 比赛密码（如果公开则留空）
+     */
     private Date beginTime;
     private Integer length;
     private String password;
-    private String description;
-    private String contestType;
-    private Integer reward;
 
-    public ContestInfo(Integer createrId, String title, Date beginTime, Integer length,
-                       String password, String description, String contestType, Integer reward) {
-        this.createrId = createrId;
+    /**
+     * 比赛描述
+     */
+    private String description;
+
+    /**
+     * 比赛类型
+     * 根据比赛类型的不同划分奖励规则
+     */
+    private ContestType contestType;
+
+    public ContestInfo(User creater, String title, Date beginTime, Integer length,
+                       String password, String description, ContestType contestType) {
+        this.creater = creater;
         this.title = title;
         this.beginTime = beginTime;
         this.length = length;
         this.password = password;
         this.description = description;
         this.contestType = contestType;
-        this.reward = reward;
     }
 
+    @Id
+    @GeneratedValue
     public Integer getContestId() {
         return contestId;
     }
 
-    public Integer getCreaterId() {
-        return createrId;
+    @ManyToOne
+    @JoinColumn(name = "creater")
+    public User getCreaterId() {
+        return creater;
     }
 
-    public void setCreaterId(Integer createrId) {
-        this.createrId = createrId;
+    public void setCreaterId(User creater) {
+        this.creater = creater;
     }
 
+    @Column(name = "title", nullable = false, length = 100)
     public String getTitle() {
         return title;
     }
@@ -48,6 +72,7 @@ public class ContestInfo {
         this.title = title;
     }
 
+    @Column(name = "begin_time", nullable = false, columnDefinition = "TIMESTAMP")
     public Date getBeginTime() {
         return beginTime;
     }
@@ -56,6 +81,7 @@ public class ContestInfo {
         this.beginTime = beginTime;
     }
 
+    @Column(name = "length", nullable = false)
     public Integer getLength() {
         return length;
     }
@@ -64,6 +90,7 @@ public class ContestInfo {
         this.length = length;
     }
 
+    @Column(name = "password")
     public String getPassword() {
         return password;
     }
@@ -72,6 +99,7 @@ public class ContestInfo {
         this.password = password;
     }
 
+    @Column(name = "description")
     public String getDescription() {
         return description;
     }
@@ -80,19 +108,14 @@ public class ContestInfo {
         this.description = description;
     }
 
-    public String getContestType() {
+    @ManyToOne
+    @JoinColumn(name = "contest_type")
+    public ContestType getContestType() {
         return contestType;
     }
 
-    public void setContestType(String contestType) {
+    public void setContestType(ContestType contestType) {
         this.contestType = contestType;
     }
 
-    public Integer getReward() {
-        return reward;
-    }
-
-    public void setReward(Integer reward) {
-        this.reward = reward;
-    }
 }
