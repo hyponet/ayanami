08b2ffe3df82c25d789858322026a12e548df781
Coderhypo <i@ihypo.net>
2016-02-19 01:00:25 +0800

    update IUSERDAO

--- src/main/java/xyz/acmer/dao/IUserDao.java
+++ src/main/java/xyz/acmer/dao/IUserDao.java
@@ -1,7 +1,16 @@
 package xyz.acmer.dao;
 
+import xyz.acmer.entity.user.User;
+
 /**
  * Created by hypo on 16-2-15.
  */
 public interface IUserDao {
+
+    User get(Integer id);
+    Boolean update(User user);
+    Boolean save(User user);
+    Boolean delete(User user);
+    User getUserByUsername(String username);
+    User getUserByEmail(String email);
 }
