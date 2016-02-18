6f06f92f0af9e9d2924bda20a7ddebb141cb7f8f
Coderhypo <i@ihypo.net>
2016-02-19 01:58:53 +0800

    update xml

---
+++ src/main/java/xyz/acmer/service/IUserService.java
@@ -0,0 +1,7 @@
+package xyz.acmer.service;
+
+/**
+ * Created by hypo on 16-2-19.
+ */
+public class IUserService {
+}
---
+++ src/main/java/xyz/acmer/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package xyz.acmer.service.impl;
+
+/**
+ * Created by hypo on 16-2-19.
+ */
+public class UserServiceImpl {
+}
