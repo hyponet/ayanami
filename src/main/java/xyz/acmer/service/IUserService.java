3b334b436aa1961d1b7c4d6ffd25226e2782a352
Coderhypo <i@ihypo.net>
2016-02-20 00:48:10 +0800

    update service

--- src/main/java/xyz/acmer/service/IUserService.java
+++ src/main/java/xyz/acmer/service/IUserService.java
@@ -1,7 +1,11 @@
 package xyz.acmer.service;
 
+import xyz.acmer.entity.user.User;
+
 /**
  * Created by hypo on 16-2-19.
  */
 public interface IUserService {
+
+    User addNewUser(String username, String nickname, String email, String password);
 }
--- src/main/java/xyz/acmer/service/impl/UserServiceImpl.java
+++ src/main/java/xyz/acmer/service/impl/UserServiceImpl.java
@@ -2,17 +2,16 @@ package xyz.acmer.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.test.context.ContextConfiguration;
 import xyz.acmer.dao.IUserDao;
-import xyz.acmer.dao.impl.UserDaoImpl;
 import xyz.acmer.entity.user.User;
+import xyz.acmer.service.IUserService;
 import xyz.acmer.util.EncryptHelper;
 
 /**
  * Created by hypo on 16-2-19.
  */
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements IUserService {
 
     @Autowired
     private IUserDao dao;
