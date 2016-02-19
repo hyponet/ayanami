779bc6ef2191dce8d60e4bedc8d43d2f1feda645
Coderhypo <i@ihypo.net>
2016-02-20 00:49:28 +0800

    add test

--- src/test/java/xyz/acmer/service/UserServiceTest.java
+++ src/test/java/xyz/acmer/service/UserServiceTest.java
@@ -1,7 +1,26 @@
 package xyz.acmer.service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 /**
  * Created by hypo on 16-2-19.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"classpath*:spring.xml"})
 public class UserServiceTest {
+
+    @Autowired
+    private IUserService userService;
+
+    @Test
+    public void addNew(){
+
+        userService.addNewUser("test2", "test2", "test2@dss.com", "123");
+    }
 }
