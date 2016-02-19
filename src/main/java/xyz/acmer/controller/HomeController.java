25c42144378ef3f439138f2df6513a62538b74e1
Coderhypo <i@ihypo.net>
2016-02-20 00:47:53 +0800

    update controller

--- src/main/java/xyz/acmer/controller/HomeController.java
+++ src/main/java/xyz/acmer/controller/HomeController.java
@@ -1,21 +1,18 @@
 package xyz.acmer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import xyz.acmer.dao.IUserDao;
-import xyz.acmer.dao.impl.UserDaoImpl;
-import xyz.acmer.entity.user.User;
-import xyz.acmer.service.impl.UserServiceImpl;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
-		User user = new UserServiceImpl().addNewUser("hypo", "hypo", "i@ihypo.net", "123");
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
--- src/main/java/xyz/acmer/controller/UserController.java
+++ src/main/java/xyz/acmer/controller/UserController.java
@@ -1,7 +1,26 @@
 package xyz.acmer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import xyz.acmer.service.IUserService;
+
 /**
  * Created by hypo on 16-2-20.
  */
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String addUser(ModelMap model) {
+
+        model.addAttribute("message", "Hello world!");
+        return "hello";
+    }
 }
