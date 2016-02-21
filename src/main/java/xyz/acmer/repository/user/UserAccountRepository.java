5d5519f8b9d5b63eaae0cc3cc8ea357fb1aca24e
Coderhypo <i@ihypo.net>
2016-02-21 17:21:06 +0800

    update UserAccountRepository

--- src/main/java/xyz/acmer/repository/user/UserAccountRepository.java
+++ src/main/java/xyz/acmer/repository/user/UserAccountRepository.java
@@ -1,7 +1,10 @@
 package xyz.acmer.repository.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import xyz.acmer.entity.user.UserAccount;
+
 /**
  * Created by hypo on 16-2-21.
  */
-public interface UserAccountRepository {
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 }
