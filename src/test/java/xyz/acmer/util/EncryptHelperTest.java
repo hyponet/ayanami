65859636adccd117dab1189b9fe8f3cc958fd6b8
Coderhypo <i@ihypo.net>
2016-02-22 00:27:38 +0800

    test util

--- src/test/java/xyz/acmer/util/EncryptHelperTest.java
+++ src/test/java/xyz/acmer/util/EncryptHelperTest.java
@@ -1,7 +1,41 @@
 package xyz.acmer.util;
 
+import org.junit.Test;
+
 /**
  * Created by hypo on 16-2-21.
  */
 public class EncryptHelperTest {
+
+    @Test
+    public void getPassword(){
+
+        String password = "This is a Password";
+        for(int i = 0;i < 10;i++){
+            password = EncryptHelper.getPassword(password);
+            System.out.println(i + " ### " + password);
+        }
+
+        System.out.println();
+
+        password = "This is a Password";
+        for(int i = 0;i < 10;i++){
+            password = EncryptHelper.getPassword(password);
+            System.out.println(i + " ### " + password);
+        }
+    }
+
+    @Test
+    public void testMD5(){
+
+        String string = "This is a String";
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for(int i = 0;i < 50;i++){
+            stringBuffer.append(string + (i % 10 == 0 ? "\n" : " ")) ;
+        }
+
+        System.out.println(stringBuffer);
+        System.out.println(EncryptHelper.md5(string));
+    }
 }
--- src/test/java/xyz/acmer/util/JsonSenderTest.java
+++ src/test/java/xyz/acmer/util/JsonSenderTest.java
@@ -1,7 +1,28 @@
 package xyz.acmer.util;
 
+import org.json.JSONObject;
+import org.junit.Test;
+
 /**
  * Created by hypo on 16-2-21.
  */
 public class JsonSenderTest {
+
+    @Test
+    public void senderGET(){
+        JSONObject jsonObject = JsonSender.get("http://127.0.0.1:5000/poj/problem/1000");
+
+        System.out.println(jsonObject.get("problem_id") + "\n" + jsonObject);
+    }
+
+    @Test
+    public void senderPOST(){
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("password", "sdutacm");
+
+        JSONObject userInfo = JsonSender.post("http://127.0.0.1:5000/poj/user/sdutacm1", jsonObject);
+
+        System.out.println(userInfo);
+    }
 }
