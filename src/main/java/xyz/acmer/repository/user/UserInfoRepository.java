1b127b982247d978d480946e76d7566f49c630d8
Coderhypo <i@ihypo.net>
2016-02-21 17:21:21 +0800

    UserInfoRepository

--- src/main/java/xyz/acmer/repository/user/UserInfoRepository.java
+++ src/main/java/xyz/acmer/repository/user/UserInfoRepository.java
@@ -1,7 +1,10 @@
 package xyz.acmer.repository.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import xyz.acmer.entity.user.UserInfo;
+
 /**
  * Created by hypo on 16-2-21.
  */
-public interface UserInfoRepository {
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
 }
