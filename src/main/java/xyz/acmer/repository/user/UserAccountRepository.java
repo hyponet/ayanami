5bdf8ff8f8fa2b4411fc9fbd55e117b4851db9ed
Coderhypo <i@ihypo.net>
2016-02-22 00:57:29 +0800

    test user account

--- src/main/java/xyz/acmer/repository/user/UserAccountRepository.java
+++ src/main/java/xyz/acmer/repository/user/UserAccountRepository.java
@@ -1,10 +1,16 @@
 package xyz.acmer.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import xyz.acmer.entity.user.User;
 import xyz.acmer.entity.user.UserAccount;
 
 /**
  * Created by hypo on 16-2-21.
  */
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+
+    @Query("SELECT a FROM UserAccount a WHERE a.user=:user and a.ojCode=:ojCode")
+    UserAccount getAcciuntByUserAndOj(@Param("user")User user, @Param("ojCode")String ojCode);
 }
--- src/test/java/xyz/acmer/database/UserAccountTest.java
+++ src/test/java/xyz/acmer/database/UserAccountTest.java
@@ -1,7 +1,58 @@
 package xyz.acmer.database;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import xyz.acmer.entity.user.User;
+import xyz.acmer.entity.user.UserAccount;
+import xyz.acmer.repository.user.UserAccountRepository;
+import xyz.acmer.repository.user.UserRepository;
+
 /**
  * Created by hypo on 16-2-22.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"classpath*:spring.xml"})
 public class UserAccountTest {
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+    private UserAccount userAccount;
+
+    @Before
+    public void getUser(){
+        this.user = userRepository.getUserByUsername("hypo");
+    }
+
+    @After
+    public void printInfo(){
+        System.out.println(userAccount.getAccountId() + " " +
+                userAccount.getUser().getUserName() + userAccount.getLoginName());
+    }
+
+    @Test
+    public void addAccount(){
+
+        this.userAccount = new UserAccount(user, "poj", "sdutacm1", "sdutacm", 10, 20);
+        userAccountRepository.save(userAccount);
+    }
+
+    @Test
+    public void updateAccount(){
+        this.userAccount = userAccountRepository.getAcciuntByUserAndOj(user, "poj");
+
+        userAccount.setSubmit(50);
+        userAccountRepository.save(userAccount);
+    }
 }
