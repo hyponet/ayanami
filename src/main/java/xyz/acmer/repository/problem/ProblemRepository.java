9ecc20c975aed1d8176da5557b015ce304b89c3d
Coderhypo <i@ihypo.net>
2016-02-21 17:20:51 +0800

    update ProblemRepository

--- src/main/java/xyz/acmer/repository/problem/ProblemRepository.java
+++ src/main/java/xyz/acmer/repository/problem/ProblemRepository.java
@@ -1,7 +1,10 @@
 package xyz.acmer.repository.problem;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import xyz.acmer.entity.problem.Problem;
+
 /**
  * Created by hypo on 16-2-21.
  */
-public interface ProblemRepository {
+public interface ProblemRepository extends JpaRepository<Problem, Long> {
 }
