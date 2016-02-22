bb81429d97d77011afadf6beb2947739840a3614
Coderhypo <i@ihypo.net>
2016-02-23 00:26:23 +0800

    add makinamilist and SystemAnnouncement

--- src/main/java/xyz/acmer/entity/system/MakinamiList.java
+++ src/main/java/xyz/acmer/entity/system/MakinamiList.java
@@ -1,7 +1,64 @@
 package xyz.acmer.entity.system;
 
+import javax.persistence.*;
+
 /**
+ * Ayanami依赖的微服务系统Makinami列表
  * Created by hypo on 16-2-22.
  */
+@Entity
+@Table(name = "makinami_list")
 public class MakinamiList {
+
+    private Integer id;
+    private String url;
+    private String type;
+    private Long useTimes = 0l;
+
+    public MakinamiList(){
+
+    }
+
+    public MakinamiList(String url, String type) {
+        this.url = url;
+        this.type = type;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "url", nullable = false, length = 100)
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Column(name = "type", nullable = false, length = 15)
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Column(name = "use_times")
+    public Long getUseTimes() {
+        return useTimes;
+    }
+
+    public void setUseTimes(Long useTimes) {
+        this.useTimes = useTimes;
+    }
 }
--- src/main/java/xyz/acmer/entity/system/SystemAnnouncement.java
+++ src/main/java/xyz/acmer/entity/system/SystemAnnouncement.java
@@ -1,7 +1,96 @@
 package xyz.acmer.entity.system;
 
+import xyz.acmer.entity.user.User;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
+ * 系统公告表
  * Created by hypo on 16-2-22.
  */
+@Entity
+@Table(name = "announcement")
 public class SystemAnnouncement {
+
+    private Long id;
+
+    /**
+     * 标题
+     * 会及时推送标题到页面，不可为空
+     */
+    private String title;
+
+    /**
+     * 公告内容
+     */
+    private String content;
+
+    /**
+     * 署名
+     */
+    private String autherName;
+
+    /**
+     * 发布时间 自动生成
+     */
+    private Date submitTime;
+
+    public SystemAnnouncement() {
+    }
+
+    public SystemAnnouncement(String title, String content, User auther) {
+        this.title = title;
+        this.content = content;
+        this.autherName = auther.getUserName();
+
+        this.submitTime = new Date();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "title", nullable = false, length = 100)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "content")
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Column(name = "auther", nullable = false, length = 20)
+    public String getAutherName() {
+        return autherName;
+    }
+
+    public void setAutherName(String autherName) {
+        this.autherName = autherName;
+    }
+
+    @Column(name = "submit_time", columnDefinition = "TIMESTAMP")
+    public Date getSubmitTime() {
+        return submitTime;
+    }
+
+    public void setSubmitTime(Date submitTime) {
+        this.submitTime = submitTime;
+    }
 }
