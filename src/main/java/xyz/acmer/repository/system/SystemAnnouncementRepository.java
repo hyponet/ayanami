5d7dabfe390ee250e5a0a453246f6f6c1d59f73a
Coderhypo <i@ihypo.net>
2016-02-24 22:02:47 +0800

    fifish SystemAnnouncementRepository dao

--- src/main/java/xyz/acmer/repository/system/SystemAnnouncementRepository.java
+++ src/main/java/xyz/acmer/repository/system/SystemAnnouncementRepository.java
@@ -3,11 +3,13 @@ package xyz.acmer.repository.system;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import xyz.acmer.entity.system.SystemAnnouncement;
 
 /**
+ * 系统公告DAO
  * Created by hypo on 16-2-24.
  */
 @Repository
 @Transactional
-public interface SystemAnnouncementRepository extends JpaRepository<Sys{
+public interface SystemAnnouncementRepository extends JpaRepository<SystemAnnouncement, Long>{
 }
