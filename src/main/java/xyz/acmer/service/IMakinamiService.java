291ee32db077a16635ab61fb03a83a2eb88850c2
Coderhypo <i@ihypo.net>
2016-02-25 16:01:59 +0800

    update makinami service

--- src/main/java/xyz/acmer/service/IMakinamiService.java
+++ src/main/java/xyz/acmer/service/IMakinamiService.java
@@ -1,26 +1,27 @@
 package xyz.acmer.service;
 
 import xyz.acmer.entity.system.MakinamiList;
+import xyz.acmer.entity.user.UserAccount;
 
 import java.util.List;
 
 /**
- * Makinami服务层接口 完成对makinami url的管理，使用
+ * Makinami服务层接口 完成对makinami 的 管理 交互
  * Created by hypo on 16-2-22.
  */
 public interface IMakinamiService {
 
+    /**
+     * 对makinami的管理
+     */
     MakinamiList addInternal(String url);
-
     MakinamiList addExternal(String url);
-
-    String getInternal();
-
-    String getExternal();
-
     List<MakinamiList> getAll();
-
     MakinamiList update(Integer id, String url);
-
     Boolean delete(Integer id);
+
+    /**
+     * 和makinami的交互
+     */
+    UserAccount getUserAccount(String loginname, String password, String ojCode);
 }
--- src/main/java/xyz/acmer/service/impl/MakinamiServiceImpl.java
+++ src/main/java/xyz/acmer/service/impl/MakinamiServiceImpl.java
@@ -1,11 +1,14 @@
 package xyz.acmer.service.impl;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import xyz.acmer.entity.system.MakinamiList;
+import xyz.acmer.entity.user.UserAccount;
 import xyz.acmer.repository.system.MakinamiRepository;
 import xyz.acmer.service.IMakinamiService;
+import xyz.acmer.util.JsonSender;
 
 import java.util.List;
 
@@ -47,8 +50,7 @@ public class MakinamiServiceImpl implements IMakinamiService {
      * 获得墙内爬虫
      * @return
      */
-    @Override
-    public String getInternal() {
+    private String getInternal() {
 
         List<MakinamiList> lists = makinamiRepository.getMakinamiListByType("internal");
 
@@ -73,8 +75,7 @@ public class MakinamiServiceImpl implements IMakinamiService {
      * 获得墙外爬虫
      * @return
      */
-    @Override
-    public String getExternal() {
+    private String getExternal() {
 
         List<MakinamiList> lists = makinamiRepository.getMakinamiListByType("external");
 
@@ -124,5 +125,30 @@ public class MakinamiServiceImpl implements IMakinamiService {
         }
 
         return true;
+    }
+
+    @Override
+    public UserAccount getUserAccount(String loginname, String password, String ojCode) {
+        /**
+         * 怎么选择爬虫是个问题，现在默认使用墙内爬虫
+         * 考虑要不要写一个爬虫选择工具 ==>根据ojCode选择墙内墙外爬虫（暂时感觉没必要）
+         */
+        String url = getInternal() + "/" + ojCode + "/user/" + loginname;
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("password", password);
+
+        /**
+         * 这个方法可能会奇慢无比（与用户AC数成正相关），单线程肯定会拖垮前端界面
+         */
+        jsonObject = JsonSender.post(url, jsonObject);
+
+        if(jsonObject != null && jsonObject.getBoolean("username")){
+            UserAccount account = new UserAccount(ojCode, loginname, password,
+                    jsonObject.getInt("accept"), jsonObject.getInt("submit"));
+
+            return account;
+        }
+        return null;
     }
 }
