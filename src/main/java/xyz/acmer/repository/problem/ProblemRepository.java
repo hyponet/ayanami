eea35874585545338ff8e08365ba7490e5586ac1
Coderhypo <i@ihypo.net>
2016-02-27 00:05:00 +0800

    update problem

--- src/main/java/xyz/acmer/repository/problem/ProblemRepository.java
+++ src/main/java/xyz/acmer/repository/problem/ProblemRepository.java
@@ -3,12 +3,16 @@ package xyz.acmer.repository.problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import xyz.acmer.entity.problem.Problem;
 import xyz.acmer.entity.system.OjCode;
 
 /**
  * Created by hypo on 16-2-21.
  */
+@Repository
+@Transactional
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
 
     @Query("SELECT p FROM Problem p WHERE p.ojCode=:ojCode AND p.pid=:pid")
--- src/main/java/xyz/acmer/repository/problem/StatusRepository.java
+++ src/main/java/xyz/acmer/repository/problem/StatusRepository.java
@@ -1,11 +1,24 @@
 package xyz.acmer.repository.problem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import xyz.acmer.entity.contest.ContestInfo;
+import xyz.acmer.entity.problem.Problem;
 import xyz.acmer.entity.problem.Status;
+
+import java.util.List;
 
 /**
  * 状态（Status）DAO
  * Created by hypo on 16-2-26.
  */
+@Repository
+@Transactional
 public interface StatusRepository extends JpaRepository<Status, Long> {
+
+    @Query("SELECT s FROM Status s WHERE s.contestInfo=:contest AND s.problem=:problem")
+    List<Status> getStatusByContestInfoAndProblem(@Param("contest")ContestInfo contest, @Param("problem")Problem problem);
 }
