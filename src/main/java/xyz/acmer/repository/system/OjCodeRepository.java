e74bef1bfed70b0cefb8cff238f44c36b8f5ffea
Coderhypo <i@ihypo.net>
2016-02-27 00:04:50 +0800

    update system

--- src/main/java/xyz/acmer/repository/system/OjCodeRepository.java
+++ src/main/java/xyz/acmer/repository/system/OjCodeRepository.java
@@ -3,11 +3,15 @@ package xyz.acmer.repository.system;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import xyz.acmer.entity.system.OjCode;
 
 /**
  * Created by hypo on 16-2-27.
  */
+@Repository
+@Transactional
 public interface OjCodeRepository extends JpaRepository<OjCode, Integer> {
 
     @Query("SELECT o FROM OjCode o WHERE o.ojName=:name")
