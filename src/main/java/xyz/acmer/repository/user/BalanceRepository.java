febea8ff512e23ff42598184ecd08946563b78d5
Coderhypo <i@ihypo.net>
2016-02-27 00:04:42 +0800

    update user

--- src/main/java/xyz/acmer/repository/user/BalanceRepository.java
+++ src/main/java/xyz/acmer/repository/user/BalanceRepository.java
@@ -1,7 +1,22 @@
 package xyz.acmer.repository.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import xyz.acmer.entity.user.Balance;
+import xyz.acmer.entity.user.User;
+
+import java.util.List;
+
 /**
  * Created by hypo on 16-2-26.
  */
-public interface BalanceRepository {
+@Repository
+@Transactional
+public interface BalanceRepository extends JpaRepository<Balance, Long>{
+
+    @Query("SELECT b FROM Balance b WHERE b.user=:user")
+    List<Balance> getBalanceByUser(@Param("user")User user);
 }
