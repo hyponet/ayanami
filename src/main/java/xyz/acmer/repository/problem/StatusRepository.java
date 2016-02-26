5a9348ef04df66b25fb7038efe71c6d093cd9fb9
Coderhypo <i@ihypo.net>
2016-02-26 18:06:50 +0800

    add repository

--- src/main/java/xyz/acmer/repository/problem/StatusRepository.java
+++ src/main/java/xyz/acmer/repository/problem/StatusRepository.java
@@ -1,7 +1,11 @@
 package xyz.acmer.repository.problem;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import xyz.acmer.entity.problem.Status;
+
 /**
+ * 状态（Status）DAO
  * Created by hypo on 16-2-26.
  */
-public interface StatusRepository {
+public interface StatusRepository extends JpaRepository<Status, Long> {
 }
--- src/main/java/xyz/acmer/repository/system/OjCodeRepository.java
+++ src/main/java/xyz/acmer/repository/system/OjCodeRepository.java
@@ -1,7 +1,15 @@
 package xyz.acmer.repository.system;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import xyz.acmer.entity.system.OjCode;
+
 /**
  * Created by hypo on 16-2-27.
  */
-public interface OjCodeRepository {
+public interface OjCodeRepository extends JpaRepository<OjCode, Integer> {
+
+    @Query("SELECT o FROM OjCode o WHERE o.ojName=:name")
+    OjCode getOjCodeByName(@Param("name")String name);
 }
