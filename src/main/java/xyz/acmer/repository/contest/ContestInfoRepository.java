7f6dfa0ce4f7a7b3d3fca3ee0825433dd32cfc94
Coderhypo <i@ihypo.net>
2016-02-27 14:54:10 +0800

    update repository

--- src/main/java/xyz/acmer/repository/contest/ContestInfoRepository.java
+++ src/main/java/xyz/acmer/repository/contest/ContestInfoRepository.java
@@ -1,9 +1,15 @@
 package xyz.acmer.repository.contest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import xyz.acmer.entity.contest.ContestInfo;
+import xyz.acmer.entity.contest.ContestType;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * 比赛信息DAO
@@ -12,4 +18,19 @@ import xyz.acmer.entity.contest.ContestInfo;
 @Repository
 @Transactional
 public interface ContestInfoRepository extends JpaRepository<ContestInfo, Long> {
+
+    @Query("SELECT c FROM ContestInfo c ORDER BY c.beginTime DESC")
+    List<ContestInfo> getAllContestByBeginTime();
+
+    @Query("SELECT c FROM ContestInfo c WHERE c.contestType=:type ORDER BY c.beginTime DESC")
+    List<ContestInfo> getContestByContestType(@Param("type")ContestType contestType);
+
+    @Query("SELECT c FROM ContestInfo c WHERE c.beginTime>:now ORDER BY c.beginTime DESC")
+    List<ContestInfo> getContestScheduled(@Param("date")Date now);
+
+    @Query("SELECT c FROM ContestInfo c WHERE c.endTime>:now AND c.beginTime<:now ORDER BY c.beginTime DESC")
+    List<ContestInfo> getContestRunning(@Param("date")Date now);
+
+    @Query("SELECT c FROM ContestInfo c WHERE c.endTime<:now ORDER BY c.beginTime DESC")
+    List<ContestInfo> getContestEnded(@Param("date")Date now);
 }
