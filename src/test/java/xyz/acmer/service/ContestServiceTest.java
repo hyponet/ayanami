5b57f4f7522e069525d32b86acc23a25c4264f27
Coderhypo <i@ihypo.net>
2016-02-29 19:09:38 +0800

    ContestServiceTest

--- src/test/java/xyz/acmer/service/ContestServiceTest.java
+++ src/test/java/xyz/acmer/service/ContestServiceTest.java
@@ -1,7 +1,16 @@
 package xyz.acmer.service;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 /**
+ * 比赛服务层测试
  * Created by hypo on 16-2-27.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"classpath*:spring.xml"})
 public class ContestServiceTest {
 }
